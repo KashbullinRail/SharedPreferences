c0afde8ceb33eb64f792ce705684ea398da31ed3
KashbullinRail <kashbullinrail@gmail.com>
2022-11-24 17:52:33 +0300

    добавлено поле вывода сохраненного SharedPreferences

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -14,12 +14,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var preferences: SharedPreferences
 
+    private val preferencesListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { preferences, key ->
+            if (key == PREF_KEY) {
+                binding.tvSharedPreferPrev.text = preferences.getString(key, "")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
         preferences = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE)
-        binding.etTextForSave.setText(preferences.getString(PREF_KEY, ""))
+        val currentValue = preferences.getString(PREF_KEY, "")
+
+        binding.etTextForSave.setText(currentValue)
+        binding.tvSharedPreferPrev.text = currentValue
 
         binding.btnShared.setOnClickListener {
             val value = binding.etTextForSave.text.toString()
@@ -27,5 +37,13 @@ class MainActivity : AppCompatActivity() {
                 .putString(PREF_KEY, value)
                 .apply()
         }
+        preferences.registerOnSharedPreferenceChangeListener(preferencesListener)
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        preferences.unregisterOnSharedPreferenceChangeListener(preferencesListener)
+    }
+
 }
